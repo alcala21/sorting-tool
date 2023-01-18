f18883cfe2d664325b234b55bd8c02d7ef6dd942
Carlos <alcala21@gmail.com>
2023-01-17 21:14:05 -0600

    Stage 3/6. Sorting it out.

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import sys
+import argparse
 
 
 class SortingTool:
-    def __init__(self, data_type="word"):
+    def __init__(self, data_type="word", sorting_type="natural"):
         self.data = []
-        self.type = data_type
-        self.max_val = -float("inf") if self.type == "long" else ""
+        self.dtype = data_type
+        self.stype = sorting_type
+        self.max_val = -float("inf") if self.dtype == "long" else ""
         self.max_count = 0
         self.count = 0
-        self.sorting = {"word": self.word, "line": self.line, "long": self.long}
+        self.value_counts = {}
+        self.values = []
+        self.process = {
+            "word": self.word,
+            "line": self.line,
+            "long": self.long,
+        }
         self.keywords = {
             "long": {
                 "plural": "numbers",
@@ -18,11 +26,15 @@ class SortingTool:
             "line": {"plural": "lines", "adjective": "longest", "singular": "line"},
             "word": {"plural": "words", "adjective": "longest", "singular": "word"},
         }
+        self.integers = []
 
     def start(self):
-        self.get_input()
-        self.sorting[self.type]()
-        self.print()
+        # self.get_input()
+        self.data = ["1 -2   33 4", "42", "1                 1"]
+        if self.data:
+            self.process[self.dtype]()
+            self.sort_values()
+            self.print()
 
     def get_input(self):
         while True:
@@ -46,7 +58,8 @@ class SortingTool:
     def line(self):
         self.process_array(self.data)
 
-    def max(self, arr):
+    @staticmethod
+    def max(arr):
         if isinstance(arr[0], int):
             return max(arr)
         max_len = max(map(len, arr))
@@ -54,38 +67,66 @@ class SortingTool:
         return max(same_length)
 
     def new_max_value(self, value):
-        if self.type == "long":
+        if self.dtype == "long":
             return value > self.max_val
         return len(value) > len(self.max_val) or (
             len(value) == len(self.max_val) and value > self.max_val
         )
 
     def process_array(self, arr):
-        self.count += len(arr)
-        max_val = self.max(arr)
-        max_count = arr.count(max_val)
-        if self.new_max_value(max_val):
-            self.max_val = max_val
-            self.max_count = max_count
-        elif max_val == self.max_val:
-            self.max_count = max_count
+        self.values += arr
+        for val in arr:
+            if self.value_counts.get(val, 0) == 0:
+                self.value_counts[val] = 1
+            else:
+                self.value_counts[val] += 1
+
+    def sort_values(self):
+        self.values.sort()
+        sorted_counts = dict()
+        sorted_nums = []
+        for key, val in self.value_counts.items():
+            if not sorted_counts.get(val, None):
+                sorted_counts[val] = {key}
+            else:
+                sorted_counts[val].add(key)
+        unique_counts = sorted(sorted_counts)
+        for count in unique_counts:
+            for num in sorted(sorted_counts[count]):
+                sorted_nums.append(num)
+        self.value_counts = {x: self.value_counts[x] for x in sorted_nums}
 
     def print(self):
-        first_line = f"Total {self.keywords[self.type]['plural']}: {self.count}."
-        second_line = f"The {self.keywords[self.type]['adjective']} {self.keywords[self.type]['singular']}:"
-        second_line += '\n' if self.type == 'line' else ' '
-        second_line += f"{self.max_val}"
-        second_line += '\n' if self.type == 'line' else ' '
-        second_line += (
-            f"({self.max_count} time(s), {int(self.max_count/self.count * 100)}%)."
-        )
+        self.count = len(self.values)
+        first_line = f"Total {self.keywords[self.dtype]['plural']}: {self.count}."
+        second_line = ""
+        if self.stype == "natural":
+            char = '\n' if self.dtype == 'line' else ' '
+            second_line += f"Sorted data:{char}{char.join(map(str, self.values))}"
+        else:
+            for value, count in self.value_counts.items():
+                second_line += f"{value}: {count} time(s), {int(count/self.count * 100)}%\n"
+            # second_line = f"The {self.keywords[self.dtype]['adjective']} {self.keywords[self.dtype]['singular']}:"
+            # second_line += "\n" if self.dtype == "line" else " "
+            # second_line += f"{self.max_val}"
+            # second_line += "\n" if self.dtype == "line" else " "
+            # second_line += (
+            #     f"({self.max_count} time(s), {int(self.max_count/self.count * 100)}%)."
+            # )
         print(first_line)
         print(second_line)
 
 
-data_type = "word"
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        data_type = sys.argv[2]
-st = SortingTool(data_type)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-dataType", choices=["word", "long", "line"], default="word")
+    parser.add_argument(
+        "-sortingType", choices=["natural", "byCount"], default="natural"
+    )
+    args = parser.parse_args()
+    sorting_type = args.sortingType
+    data_type = args.dataType
+data_type = "line"
+sorting_type = "natural"
+st = SortingTool(data_type, sorting_type)
 st.start()
